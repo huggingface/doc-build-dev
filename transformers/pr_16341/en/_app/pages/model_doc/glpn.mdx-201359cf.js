9033ac1632a99c79973e556739f349e230bfe6a8
Hugging Face Doc Builder <docs@huggingface.co>
2022-03-30 12:54:40 +0000

    Updated with commit ee5875fc9659ce5bef73c154e043c5543773de54 See: https://github.com/huggingface/transformers/commit/ee5875fc9659ce5bef73c154e043c5543773de54

--- transformers/pr_16341/en/_app/pages/model_doc/glpn.mdx-201359cf.js
+++ transformers/pr_16341/en/_app/pages/model_doc/glpn.mdx-201359cf.js
@@ -98,10 +98,8 @@ elements depending on the configuration (<a
 <p><strong>last_hidden_state</strong> (<code>torch.FloatTensor</code> of shape <code>(batch_size, sequence_length, hidden_size)</code>) \u2014 Sequence of hidden-states at the output of the last layer of the model.</p>
 </li>
 <li>
-<p><strong>hidden_states</strong> (<code>tuple(torch.FloatTensor)</code>, <em>optional</em>, returned when <code>output_hidden_states=True</code> is passed or when <code>config.output_hidden_states=True</code>) \u2014 Tuple of <code>torch.FloatTensor</code> (one for the output of the embeddings, if the models has an embedding layer,,</p>
-<ul>
-<li>one for the output of each layer) of shape <code>(batch_size, sequence_length, hidden_size)</code>.</li>
-</ul>
+<p><strong>hidden_states</strong> (<code>tuple(torch.FloatTensor)</code>, <em>optional</em>, returned when <code>output_hidden_states=True</code> is passed or when <code>config.output_hidden_states=True</code>) \u2014 Tuple of <code>torch.FloatTensor</code> (one for the output of the embeddings, if the models has an embedding layer, +
+one for the output of each layer) of shape <code>(batch_size, sequence_length, hidden_size)</code>.</p>
 <p>Hidden-states of the model at the output of each layer plus the initial embedding outputs.</p>
 </li>
 <li>
